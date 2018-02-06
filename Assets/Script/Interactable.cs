81e5375ff10439afb3c5c57e7798b9f3bae325f6
Anthony Mcgarry <anthony.mcgarry@wearenova.co.uk>
2018-02-06 17:37:17 +0000

    Able to interact with objects and follow objects movements

---
+++ Assets/Script/Interactable.cs
@@ -0,0 +1,52 @@
+﻿
+using UnityEngine;
+
+public class Interactable : MonoBehaviour {
+
+    public float radius = 3f;
+    public Transform interactionTranform;
+    bool isFocus = false;
+    Transform player;
+
+    bool hasInteracted = false;
+
+    public virtual void Interact ()
+    {
+        // This method will be overwritten depending on the object
+        Debug.Log("Interacting with " + transform.name);
+    }
+
+    private void Update()
+    {
+        if (isFocus && !hasInteracted) {
+
+            float distance = Vector3.Distance(player.position, interactionTranform.position);
+            if(distance <= radius)
+            {
+                Interact();
+                hasInteracted = true;
+            }
+        }
+    }
+
+    public void OnFocus( Transform playerTransform)
+    {
+        isFocus = true;
+        player = playerTransform;
+        hasInteracted = false;
+    }
+
+    public void OnDefocus()
+    {
+        isFocus = false;
+        player = null;
+        hasInteracted = false;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(interactionTranform.position, radius);
+    }
+
+}
--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 [RequireComponent(typeof(PlayerMotor))]
 public class PlayerController : MonoBehaviour {
 
+    public Interactable focus;
+
     public LayerMask movementMask;
 
     Camera cam;
@@ -26,6 +28,8 @@ public class PlayerController : MonoBehaviour {
             if(Physics.Raycast(ray, out hit, 100, movementMask))
             {
                 motor.MoveToPoint(hit.point);
+
+                RemoveFocus();
             }
         }
 
@@ -37,7 +41,40 @@ public class PlayerController : MonoBehaviour {
             if (Physics.Raycast(ray, out hit, 100))
             {
                 //Check if we hit an interactable object
+                Interactable interactable = hit.collider.GetComponent<Interactable>();
+
+                // if object is hit set to focus on object
+
+                if (interactable)
+                {
+                    SetFocus(interactable);
+                }
             }
         }
+    }
+
+    void SetFocus(Interactable newFocus)
+    {
+        if(newFocus != focus)
+        {
+            if (focus)
+                focus.OnDefocus();
+
+            focus = newFocus;
+            motor.FollowTarget(newFocus);
+        }
+
+        
+        newFocus.OnFocus(transform);
+
+    }
+
+    void RemoveFocus()
+    {
+        if (focus)
+            focus.OnDefocus();
+
+        focus = null;
+        motor.StopFollowingTarget();
     }
 }

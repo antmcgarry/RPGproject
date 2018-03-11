cdff4ab05ef6e1968d6e2387974984149f14b30d
Anthony Mcgarry <antjmcgarry@gmail.com>
2018-03-11 15:38:52 +0000

    Added a Player UI in the top corner for health and energy

---
+++ Assets/HealthManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthManager : MonoBehaviour {
+
+    public Image healthBar;
+    public Image enegyBar;
+    public CharacterStats stats;
+
+
+	// Use this for initialization
+	void Start () {
+        stats = GetComponent<CharacterStats>();
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        float healthPercent = GetStatsPercent(stats.currentHealth, stats.maxHealth);
+        healthBar.fillAmount = healthPercent;
+	}
+
+    float GetStatsPercent( int currentStat, int maxStat)
+    {
+        return Mathf.Clamp01(currentStat / (float)maxStat);
+    }
+}
